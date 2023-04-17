60eb604ac3e9d30d6d505367f50c2f9f39d70325
Fahyvor <favourokafor30@gmail.com>
2023-04-17 01:33:45 +0100

    First commit

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,14 +4,16 @@ import OrderImage from '../../assets/Order.jpeg'
 
 const Order = () => {
   return (
-    <div className='order-container d-flex
-    gap-4 pt-3 pb-4
+    <div className='container-fluid order-container d-flex
+    gap-4 pt-3 pb-5
     justify-content-around'>
-     <div className='order-image'>
-      <img src={OrderImage} />
+     <div className='order-image-container mt-3'>
+      <img src={OrderImage} className='order-image mt-1'/>
      </div>
      <div className='order-writeup'>
-
+      <h1 className='text-white text-center order-text'>
+        ORDER a <br/>Pot/Plate <br/> from US
+        </h1>
      </div>
     </div>
   )
