e9deea1d608fcedc59c87d32b57e0502e4652301
Fahyvor <favourokafor30@gmail.com>
2023-04-30 15:21:21 +0100

    First commit

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,8 +6,7 @@ import Background from '../../assets/background.jpeg'
 const Body = () => {
   return (
    <>
-    <div id='body-container' className='d-flex p-2 pt-4
-    data-aos-fade-up data-aos-duration-500 justify-content-around py-4'>
+    <div id='body-container' className='d-flex p-2 pt-4 justify-content-around py-4'>
      <div id='left-side' className='my-auto'>
       <h1 className='text-white company-name'>Lorem Ipsum</h1>
       <div className='text-white flex-column'>
@@ -16,10 +15,10 @@ const Body = () => {
       </div>
       <div id='order-buttons' className='d-flex gap-4 mx-2'>
        <div className='align-items-center
-       p-3 px-5 text-white' id='place_order' ><a className='text-white'>
+       p-2 px-5 text-white' id='place_order' ><a className='text-white'>
         Place Order</a>
         </div>
-       <div className='align-items-center bg-white p-3 px-5 contact-us'><a>Contact Us</a></div>
+       <div className='align-items-center bg-white p-2 px-5 contact-us'><a>Contact Us</a></div>
       </div>
      </div>
      <div className='right-side align-items-center justify-content-center'>
